ba001d11672195b8dbfab218c86dc5a74dba82a6
TopSensor <eugenio@esg.eng.br>
2020-09-06 12:51:13 -0300

    baudrate 9600

--- uart.js
+++ uart.js
@@ -239,7 +239,7 @@
       navigator.serial.requestPort({}).then(function(port) {
         log(1, "Connecting to serial port");
         serialPort = port;
-        return port.open({ baudrate: 115200 });
+        return port.open({ baudrate: 9600 });
       }).then(function () {
         function readLoop() {
           var reader = serialPort.readable.getReader();
